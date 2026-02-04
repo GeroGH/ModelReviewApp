6345532534380c09f638c0ed5eae2f501eb38515
Gergan Gospodinov <gospodinovgergan@gmail.com>
2026-02-04 16:47:29 +0000

    Add project files.

---
+++ ModelReviewForm.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace ModelReviewApp
+{
+    partial class ModelReviewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ReadyToBeConected = new System.Windows.Forms.Button();
+            this.UnderReviewType1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.UnderReviewType3 = new System.Windows.Forms.Button();
+            this.UnderReviewType2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.ReadyForEditting = new System.Windows.Forms.Button();
+            this.TempWorksReady = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.Stage4Import = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ReadyToBeConected
+            // 
+            this.ReadyToBeConected.BackColor = System.Drawing.Color.LimeGreen;
+            this.ReadyToBeConected.Location = new System.Drawing.Point(6, 19);
+            this.ReadyToBeConected.Name = "ReadyToBeConected";
+            this.ReadyToBeConected.Size = new System.Drawing.Size(103, 51);
+            this.ReadyToBeConected.TabIndex = 1;
+            this.ReadyToBeConected.Text = "GAs Approved";
+            this.ReadyToBeConected.UseVisualStyleBackColor = false;
+            this.ReadyToBeConected.Click += new System.EventHandler(this.ReadyToBeConected_Click);
+            // 
+            // UnderReviewType1
+            // 
+            this.UnderReviewType1.BackColor = System.Drawing.Color.Orange;
+            this.UnderReviewType1.Location = new System.Drawing.Point(6, 17);
+            this.UnderReviewType1.Name = "UnderReviewType1";
+            this.UnderReviewType1.Size = new System.Drawing.Size(103, 51);
+            this.UnderReviewType1.TabIndex = 2;
+            this.UnderReviewType1.Text = "Type 1";
+            this.UnderReviewType1.UseVisualStyleBackColor = false;
+            this.UnderReviewType1.Click += new System.EventHandler(this.UnderReviewType1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.UnderReviewType1);
+            this.groupBox1.Controls.Add(this.UnderReviewType3);
+            this.groupBox1.Controls.Add(this.UnderReviewType2);
+            this.groupBox1.Location = new System.Drawing.Point(134, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(117, 192);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Stage 5.2 Review";
+            // 
+            // UnderReviewType3
+            // 
+            this.UnderReviewType3.BackColor = System.Drawing.Color.Orchid;
+            this.UnderReviewType3.Location = new System.Drawing.Point(6, 131);
+            this.UnderReviewType3.Name = "UnderReviewType3";
+            this.UnderReviewType3.Size = new System.Drawing.Size(103, 51);
+            this.UnderReviewType3.TabIndex = 4;
+            this.UnderReviewType3.Text = "Type 3";
+            this.UnderReviewType3.UseVisualStyleBackColor = false;
+            this.UnderReviewType3.Click += new System.EventHandler(this.UnderReviewType3_Click);
+            // 
+            // UnderReviewType2
+            // 
+            this.UnderReviewType2.BackColor = System.Drawing.Color.Coral;
+            this.UnderReviewType2.Location = new System.Drawing.Point(6, 74);
+            this.UnderReviewType2.Name = "UnderReviewType2";
+            this.UnderReviewType2.Size = new System.Drawing.Size(103, 51);
+            this.UnderReviewType2.TabIndex = 3;
+            this.UnderReviewType2.Text = "Type 2";
+            this.UnderReviewType2.UseVisualStyleBackColor = false;
+            this.UnderReviewType2.Click += new System.EventHandler(this.UnderReviewType2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Location = new System.Drawing.Point(380, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(118, 192);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Stage 5.3 Review";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Orange;
+            this.button1.Location = new System.Drawing.Point(6, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Type 1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Orchid;
+            this.button4.Location = new System.Drawing.Point(6, 131);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 51);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Type 3";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Coral;
+            this.button5.Location = new System.Drawing.Point(6, 74);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(103, 51);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Type 2";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // ReadyForEditting
+            // 
+            this.ReadyForEditting.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ReadyForEditting.Location = new System.Drawing.Point(6, 76);
+            this.ReadyForEditting.Name = "ReadyForEditting";
+            this.ReadyForEditting.Size = new System.Drawing.Size(103, 51);
+            this.ReadyForEditting.TabIndex = 9;
+            this.ReadyForEditting.Text = "Ready";
+            this.ReadyForEditting.UseVisualStyleBackColor = false;
+            this.ReadyForEditting.Click += new System.EventHandler(this.ReadyForEditting_Click_1);
+            // 
+            // TempWorksReady
+            // 
+            this.TempWorksReady.BackColor = System.Drawing.Color.Yellow;
+            this.TempWorksReady.Location = new System.Drawing.Point(6, 20);
+            this.TempWorksReady.Name = "TempWorksReady";
+            this.TempWorksReady.Size = new System.Drawing.Size(103, 51);
+            this.TempWorksReady.TabIndex = 9;
+            this.TempWorksReady.Text = "Ready";
+            this.TempWorksReady.UseVisualStyleBackColor = false;
+            this.TempWorksReady.Click += new System.EventHandler(this.TempWorksReady_Click_1);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.Stage4Import);
+            this.groupBox3.Location = new System.Drawing.Point(11, 66);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(118, 82);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Stage 5.1 Import";
+            // 
+            // Stage4Import
+            // 
+            this.Stage4Import.BackColor = System.Drawing.Color.LightGray;
+            this.Stage4Import.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Stage4Import.Location = new System.Drawing.Point(6, 19);
+            this.Stage4Import.Name = "Stage4Import";
+            this.Stage4Import.Size = new System.Drawing.Size(103, 51);
+            this.Stage4Import.TabIndex = 6;
+            this.Stage4Import.Text = "Imported";
+            this.Stage4Import.UseVisualStyleBackColor = false;
+            this.Stage4Import.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Stage4Import_MouseClick);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.ReadyToBeConected);
+            this.groupBox4.Location = new System.Drawing.Point(256, 66);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(118, 82);
+            this.groupBox4.TabIndex = 11;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Stage 5.2 Complete";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.TempWorksReady);
+            this.groupBox5.Controls.Add(this.ReadyForEditting);
+            this.groupBox5.Location = new System.Drawing.Point(504, 38);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(116, 133);
+            this.groupBox5.TabIndex = 12;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Stage 5.3 Complete";
+            // 
+            // ModelReviewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 211);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ModelReviewForm";
+            this.Text = "Model Review Application";
+            this.Load += new System.EventHandler(this.ModelReviewForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ReadyToBeConected;
+        private System.Windows.Forms.Button UnderReviewType1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button ReadyForEditting;
+        private System.Windows.Forms.Button TempWorksReady;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button Stage4Import;
+        private System.Windows.Forms.Button UnderReviewType3;
+        private System.Windows.Forms.Button UnderReviewType2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+    }
+}
+
---
+++ ModelReviewForm.cs
@@ -0,0 +1,116 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using Tekla.Structures.Model;
+using TSMUI = Tekla.Structures.Model.UI;
+
+namespace ModelReviewApp
+{
+    public partial class ModelReviewForm : Form
+    {
+        public ModelReviewForm()
+        {
+            this.InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.TopMost = true;
+        }
+
+        private void Stage4Import_Click(object sender, System.EventArgs e)
+        {
+            ChangeClass("1");
+        }
+
+        private static void ChangeClass(string partClass)
+        {
+            var model = new Model();
+            var mos = new TSMUI.ModelObjectSelector();
+            var moe = mos.GetSelectedObjects();
+
+            while (moe.MoveNext())
+            {
+                var part = moe.Current as Part;
+                if (part == null) { continue; }
+
+                part.Class = partClass;
+                part.Modify();
+            }
+
+            model.CommitChanges();
+        }
+
+        private void UnderReviewType1_Click(object sender, System.EventArgs e)
+        {
+            ChangeClass("13");
+        }
+
+        private void UnderReviewType2_Click(object sender, System.EventArgs e)
+        {
+            ChangeClass("2");
+        }
+
+        private void UnderReviewType3_Click(object sender, System.EventArgs e)
+        {
+            ChangeClass("9");
+        }
+
+        private void TempWorksReady_Click(object sender, System.EventArgs e)
+        {
+            ChangeClass("6");
+        }
+
+        private void ReadyToBeConected_Click(object sender, System.EventArgs e)
+        {
+            ChangeClass("3");
+        }
+
+        private void ModelReviewForm_Load(object sender, System.EventArgs e)
+        {
+            var pos = this.Location;
+            this.Location = new Point(pos.X + 570, pos.Y - 250);
+        }
+
+        //private void Stage4Import_Click_1(object sender, System.EventArgs e)
+        //{
+        //    if (e.Button == MouseButtons.Left && (Control.ModifierKeys & Keys.Control) == Keys.Control)
+        //    {
+        //        MessageBox.Show("Test");
+        //    }
+
+        //    ChangeClass("1");
+        //}
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            ChangeClass("13");
+        }
+
+        private void button5_Click(object sender, System.EventArgs e)
+        {
+            ChangeClass("2");
+        }
+
+        private void button4_Click(object sender, System.EventArgs e)
+        {
+            ChangeClass("9");
+        }
+
+        private void TempWorksReady_Click_1(object sender, System.EventArgs e)
+        {
+            ChangeClass("6");
+        }
+
+        private void ReadyForEditting_Click_1(object sender, System.EventArgs e)
+        {
+            ChangeClass("11");
+        }
+
+        private void Stage4Import_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && (Control.ModifierKeys & Keys.Control) == Keys.Control)
+            {
+                MessageBox.Show("Control + Mouse ...");
+            }
+
+            ChangeClass("1");
+        }
+    }
+}
